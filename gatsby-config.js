23e58611f1a971083c37dbcd60560b99504b3852
Alexander Muratidi <intermyr@gmail.com>
2020-07-06 15:20:46 +0300

    Add Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `news`,
-        path: `${__dirname}/src/pages/news`,
+        path: `${__dirname}/src/content`,
       },
     },
     {
@@ -44,6 +44,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
