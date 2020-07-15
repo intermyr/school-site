48960b4c12597a09cdef6203050362a138b18b38
Alexander Muratidi <intermyr@gmail.com>
2020-07-15 12:54:24 +0300

    change styling

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,6 @@
 import CMS from "netlify-cms-app"
 import { ru } from "netlify-cms-locales"
-import "../styles/cms-preview.css"
 
 CMS.registerLocale("ru", ru)
 
-CMS.registerPreviewStyle("../styles/cms-preview.css")
+CMS.registerPreviewStyle("cms.css")
